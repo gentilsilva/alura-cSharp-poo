efe2c95ea1a60e4eb234554ce77450ac6662f276
Gentil Silva <silvagentil.sg@gmail.com>
2023-06-04 05:33:27 -0300

    Aprendendo a usar dicionários e adicionando avaliação de bandas

--- 1-primeira-aplicacao/PrimeiroPrograma/PrimeiroPrograma/Program.cs
+++ 1-primeira-aplicacao/PrimeiroPrograma/PrimeiroPrograma/Program.cs
@@ -1,8 +1,9 @@
 ﻿// Screen Sound
-
 string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
-List<string> listaDasBandas = new List<string> {"U2", "The Beatles", "Calipso"};
-
+//List<string> listaDasBandas = new List<string> {"U2", "The Beatles", "Calipso"};
+Dictionary<string, List<int>> bandasRegistradas = new Dictionary<string, List<int>>();
+bandasRegistradas.Add("Linkin Park", new List<int> { 10, 8, 6 });
+bandasRegistradas.Add("The Beatles", new List<int>());
 
 void ExibirLogo()
 {
@@ -42,7 +43,7 @@ void CapturarEscolha()
             MostrarBandasRegistradas();
             break;
         case 3:
-            Console.WriteLine("Você escolheu a opção: " + opcaoEscolhidaNumerica);
+            AvaliarUmaBanda();
             break;
         case 4:
             Console.WriteLine("Você escolheu a opção: " + opcaoEscolhidaNumerica);
@@ -59,12 +60,12 @@ void CapturarEscolha()
 void RegistrarBanda()
 {
     Console.Clear();
-    Console.WriteLine("*******************");
-    Console.WriteLine("Registro de bandas.");
-    Console.WriteLine("*******************");
+    ExibirTituloDaOpcao("Registro Das Bandas");
     Console.Write("\nDigite o nome da banda que deseja registrar: ");
     string nomeDaBanda = Console.ReadLine()!;
-    listaDasBandas.Add(nomeDaBanda);
+
+    bandasRegistradas.Add(nomeDaBanda, new List<int>());
+
     Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
     Thread.Sleep(2000);
     Console.Clear();
@@ -74,18 +75,59 @@ void RegistrarBanda()
 void MostrarBandasRegistradas()
 {
     Console.Clear();
-    Console.WriteLine("*************************************");
-    Console.WriteLine("Exibindo todas as bandas registradas.");
-    Console.WriteLine("*************************************\n");
-    for (int i = 0; i < listaDasBandas.Count; i++)
+    ExibirTituloDaOpcao("Exibindo todas as bandas registradas");
+
+    foreach( string banda in bandasRegistradas.Keys)
     {
-        Console.WriteLine($"Banda: {listaDasBandas[i]}");
+        Console.WriteLine($"Banda: {banda}");
     }
 
     Console.WriteLine("\nDigite uma tecla para voltar ao menu principal.");
     Console.ReadKey();
     Console.Clear();
     ExibirOpcoesDoMenu();
+}
+
+void ExibirTituloDaOpcao(string titulo)
+{
+    int quantidadeLetras = titulo.Length;
+    string asteriscos = string.Empty.PadLeft(quantidadeLetras, '*');        // Estou criando uma string com a quantidade de asteriscos que é passado pela quantidade de letras. PadLeft adiciona caracters a esquerda
+    Console.WriteLine(asteriscos);
+    Console.WriteLine(titulo);
+    Console.WriteLine(asteriscos + "\n");
+}
+
+void AvaliarUmaBanda()
+{
+    // digite qual banda deseja avaliar
+    // se a banda existe no dicionario >> avaliar uma banda
+    // se nao existe >> volta ao menu principal
+
+    Console.Clear();
+    ExibirTituloDaOpcao("Avaliar banda");
+    Console.Write("Digite o nome da banda que deseja avaliar: ");
+    string nomeDaBanda = Console.ReadLine()!;
+
+    if(bandasRegistradas.ContainsKey(nomeDaBanda))              // Verirfica se o nomeDaBanda é uma chave existente no dicionario bandasRegistradas
+    {
+        Console.Write($"Qual a nota que a banda {nomeDaBanda} merece: ");
+        int nota = int.Parse(Console.ReadLine()!);
+
+        bandasRegistradas[nomeDaBanda].Add(nota);           // Adiciona uma nota na lista que é referenciada pela chave nomeDaBanda
+        Console.WriteLine($"\nA nota {nota} foi registrada com sucesso para a banda {nomeDaBanda}");
+        Thread.Sleep(4000);                 // Pede para esperar uma quantida de tempo antes de executar outro trabalho. Nesse caso, 2000 milissegundos.
+        Console.Clear();
+        ExibirOpcoesDoMenu();
+    } 
+    else
+    {
+        Console.WriteLine($"\nA banda {nomeDaBanda} não foi encontrada");
+        Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+        Console.ReadKey();
+        Console.Clear();
+        ExibirOpcoesDoMenu();
+    }
+
 }
 
 
