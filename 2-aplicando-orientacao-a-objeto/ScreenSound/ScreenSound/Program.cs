31e7c0373174542d2c5e03b02659cbde39c07765
Gentil Silva <silvagentil.sg@gmail.com>
2023-06-04 21:30:31 -0300

    Entendendo sobre classes, atributos e métodos

---
+++ 2-aplicando-orientacao-a-objeto/ScreenSound/ScreenSound/Program.cs
@@ -0,0 +1,14 @@
+﻿Musica musicaUm = new Musica();
+musicaUm.nome = "Roxane";
+musicaUm.artista = "The Police";
+musicaUm.duracao = 273;
+musicaUm.disponivel = true;
+
+Musica musicaDois = new Musica();
+musicaDois.nome = "Vertigo";
+musicaDois.artista = "U2";
+musicaDois.duracao = 367;
+musicaDois.disponivel = false;
+
+musicaUm.ExibirFichaTecnica();
+musicaDois.ExibirFichaTecnica();
